48cef3216b4b34ff1100e064869c32545a8db1fd
vibhavarin <vibhavari.navale77@gmail.com>
2026-06-04 14:54:08 +0200

    conftest automatisch erkannt

--- TestCases/conftest.py
+++ TestCases/conftest.py
@@ -1,22 +1,25 @@
 import pytest
-import time
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
+
 
 @pytest.fixture()
 def driver():
     opts = Options()
-    opts.add_argument('--headless')
-    opts.add_argument('--no-sandbox')
-    opts.add_argument('--disable-dev-shm-usage')
-    # opts.add_experimental_option('detach', True)
+
+    # Automatisch erkennen ob CI/CD oder Lokal
+    if os.environ.get('CI'):
+        opts.add_argument('--headless')
+        opts.add_argument('--no-sandbox')
+        opts.add_argument('--disable-dev-shm-usage')
+
     opts.add_experimental_option('prefs', {
-    'credentials_enable_service': False,
-    'profile.password_manager_enabled': False,
-    'profile.password_manager_leak_detection': False
+        'credentials_enable_service': False,
+        'profile.password_manager_enabled': False,
+        'profile.password_manager_leak_detection': False
     })
     driver = webdriver.Chrome(options=opts)
     driver.implicitly_wait(10)
     yield driver
-    driver.close()
+    driver.quit()
